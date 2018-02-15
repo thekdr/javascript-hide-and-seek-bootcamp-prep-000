d436db80070da11f1e48400244358ee3ab91f818
Kristopher Ross <thekdr@yahoo.com>
2018-02-15 21:27:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  var nested = document.querySelector("#nested");
+  return nested.querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var inc = parseInt(n);
+  var lis = document.querySelectorAll(".ranked-list li");
+  for (var i = 0; i < lis.length; i++) {
+    var newRank = parseInt(lis[i].innerHTML) + inc;
+    lis[i].innerHTML = (newRank);
+  }
+}
+
+function deepestChild() {
+  return document.querySelector("div#grand-node div div div div");
+}
